447af88ac0f034feb1b3874d02b67af98b0dca3b
Dev Bulchandani <devbulchandani8@gmail.com>
2024-08-09 16:05:14 +0530

    vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-    }),
-  ],
-})
+export default {
+  esbuild: {
+    target: 'es2022' // or 'chrome91', 'firefox90', etc.
+  }
+};
